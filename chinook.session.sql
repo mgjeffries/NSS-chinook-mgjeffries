ec66762cfa624e3c72182adf129664045daded1b
mgjeffries <gib.jeffries@gmail.com>
2020-11-23 11:53:33 -0600

    questions 2-9

--- chinook.session.sql
+++ chinook.session.sql
@@ -8,19 +8,67 @@ WHERE country != "USA";
 
 -- 2.brazil_customers.sql: Provide a query only showing the Customers from Brazil.
 
+SELECT c.FirstName || ' ' || c.LastName as FullName
+FROM Customer c
+WHERE country = "Brazil";
+
 -- 3.brazil_customers_invoices.sql: Provide a query showing the Invoices of customers who are from Brazil. The resultant table should show the customer's full name, Invoice ID, Date of the invoice and billing country.
+
+SELECT c.FirstName || ' ' || c.LastName as FullName,
+i.InvoiceId,
+i.InvoiceDate,
+i.BillingCountry
+FROM Invoice i
+    JOIN Customer c 
+    ON i.CustomerId = c.CustomerId
+WHERE country = "Brazil";
 
 -- 4 sales_agents.sql: Provide a query showing only the Employees who are Sales Agents.
 
+SELECT * 
+FROM Employee
+WHERE Title LIKE '%Sales Support Agent%';
+
 -- 5 unique_invoice_countries.sql: Provide a query showing a unique/distinct list of billing countries from the Invoice table.
+
+SELECT DISTINCT BillingCountry
+FROM Invoice;
 
 -- 6 sales_agent_invoices.sql: Provide a query that shows the invoices associated with each sales agent. The resultant table should include the Sales Agent's full name.
 
+SELECT e.FirstName || ' ' || e.LastName as EmployeeFullName,
+i.*
+FROM Invoice i
+    JOIN Customer c
+ON i.CustomerId = c.CustomerId
+    JOIN Employee e
+ON c.SupportRepId = e.EmployeeId;
+
 -- 7 invoice_totals.sql: Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+
+SELECT e.FirstName || ' ' || e.LastName as EmployeeFullName,
+c.FirstName || ' ' || c.LastName as CustomerFullName,
+i.Total,
+c.Country
+FROM Invoice i
+    JOIN Customer c
+ON i.CustomerId = c.CustomerId
+    JOIN Employee e
+ON c.SupportRepId = e.EmployeeId;
+
 
 -- 8 total_invoices_{year}.sql: How many Invoices were there in 2009 and 2011?
 
+select COUNT(InvoiceDate) 
+from Invoice
+where "2009-00-00 00:00:00" <= InvoiceDate < "2012-00-00 00:00:00";
+
 -- 9 total_sales_{year}.sql: What are the respective total sales for each of those years?
+
+SELECT SUM(Total)
+FROM Invoice
+GROUP BY STRFTIME('%Y', InvoiceDate);
+
 
 -- 10 invoice_37_line_item_count.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
 
